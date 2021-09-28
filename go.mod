fca7f1507549d4bb095d3266a5b32347a06e2da3
mukul.panwar@ibm.com <mukul.panwar@ibm.com>
2021-09-28 10:44:55 -0400

    file changed

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/mukul4u2005/matrix-service
 go 1.16
 
 require (
+	github.com/go-resty/resty/v2 v2.6.0
 	github.com/prometheus/client_golang v1.11.0
+	github.com/prometheus/common v0.26.0
+	k8s.io/api v0.22.2
 	k8s.io/apimachinery v0.22.2
 	k8s.io/client-go v0.22.2
 )
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
+/*
+Main method for scrapper is handler() function, This function calling GetPodCountsByhttp() metrix.go
+by using http call
+
+Other method podCountByNamespace is calling GetPodCount() which is using rest api
+*/
+
 import (
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 
-	"github.com/mukul4u2005/matrix-service/matrix"
+	"github.com/mukul4u2005/matrix-service/metrix"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -17,20 +25,24 @@ type PodCount struct {
 }
 
 var (
-	podCount = prometheus.NewGauge(prometheus.GaugeOpts{
+	requestsCounter uint64 = 0
+	podCount               = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "cluster_pod_count",
 		Help: "Total pods in cluster",
 	})
-	hitCounter = prometheus.NewCounter(prometheus.CounterOpts{
+	hitCounter = prometheus.NewCounterFunc(prometheus.CounterOpts{
 		Name: "hit_counter",
 		Help: "Endpoint hit count",
+	}, func() float64 {
+		return float64(atomic.LoadUint64(&requestsCounter))
 	})
+	registry = prometheus.NewRegistry()
 )
 
 func init() {
 	// Metrics have to be registered to be exposed:
-	prometheus.MustRegister(podCount)
-	prometheus.MustRegister(hitCounter)
+	registry.Register(podCount)
+	registry.Register(hitCounter)
 }
 func podCountByNamespace(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Finding pod count")
@@ -43,30 +55,35 @@ func podCountByNamespace(w http.ResponseWriter, r *http.Request) {
 	}
 	podCount := PodCount{
 		Namespace: namespace,
-		PodCount:  matrix.GetPodCount(namespace),
+		PodCount:  metrix.GetPodCount(namespace),
 	}
 	json.NewEncoder(w).Encode(podCount)
-	//hitCounter.Inc()
 
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	atomic.AddUint64(&requestsCounter, 1)
 	fmt.Fprintf(w, "Go home page")
-	//hitCounter.Inc()
 
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	atomic.AddUint64(&requestsCounter, 1)
+	var y float64 = float64(metrix.GetPodCountsByhttp())
+	podCount.Set(y)
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
 }
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/podcount", podCountByNamespace)
-	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/metrics", handler)
+	http.HandleFunc("/rest/metrics", podCountByNamespace)
 	err := http.ListenAndServe(":9000", nil)
 	log.Fatal(err)
 }
 
 func main() {
-	var y float64 = float64(matrix.GetPodCount(""))
-	podCount.Set(y)
-	hitCounter.Inc()
+
 	handleRequest()
 }
